2ad96e9c68f21b4d872bfcaa3cf74f9722ef63cb
FFLish <fflishrobotics@gmail.com>
2025-09-07 20:30:40 +0200

    Refactor main.py for improved readability and performance; remove unused functions and variables, and enhance movement functions. Delete schieben.py as it is no longer needed.

--- main.py
+++ main.py
@@ -1,49 +1,22 @@
-from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, hub_menu
 import umath as m
 from pybricks.tools import multitask, run_task
+from pybricks.hubs import PrimeHub
 
 hub = PrimeHub()
 
 hub.system.set_stop_button({Button.BLUETOOTH})
-lmA=Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
-rmB=Motor(Port.D)
-lmk=Motor(Port.F)
-rmk=Motor(Port.B)
+lmA = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
+rmB = Motor(Port.D)
+lmk = Motor(Port.F)
+rmk = Motor(Port.B)
 radius = 31.2
 drb = DriveBase(lmA, rmB, 62.4, 158)
-#drb.heading_control.enabled = True
-strecke = (m.radians(lmA.angle())*radius+m.radians(rmB.angle())*radius)/2
-alteStrecke = strecke
-x = 0
-y = 0
-var = 0
 drb.use_gyro(True)
 
-def menu(starting_number=1):
-    showing = starting_number
-    while True:
-        if hub.buttons.pressed()=={Button.LEFT}:
-            while hub.buttons.pressed()=={Button.LEFT}:
-                continue
-            showing -=1
-            print(showing)
-        elif hub.buttons.pressed()=={Button.RIGHT}:
-            while hub.buttons.pressed()=={Button.RIGHT}:
-                continue
-            showing +=1
-            print(showing)
-        elif hub.buttons.pressed()=={Button.CENTER}:
-            while hub.buttons.pressed()=={Button.CENTER}:
-                continue
-            return showing
-
-
-
-def lmkmove(distance,speed):
 def lmkmove(distance, speed):
     lmk.reset_angle(0)
     while abs(lmk.angle()) < distance:
@@ -52,13 +25,11 @@ def lmkmove(distance, speed):
             raise SystemExit("ENDE")
         if Button.CENTER in hub.buttons.pressed():
             lmk.brake()
-            wait(1)
+            wait(1000)
             raise SystemExit("ENDE GELÄNDE!")
         lmk.run(speed)
     lmk.brake()
 
-
-def rmkmove(distance,speed):
 def rmkmove(distance, speed):
     rmk.reset_angle(0)
     while abs(rmk.angle()) < distance:
@@ -67,61 +38,91 @@ def rmkmove(distance, speed):
             raise SystemExit("ENDE")
         if Button.CENTER in hub.buttons.pressed():
             rmk.brake()
-            wait(1)
+            wait(1000)
             raise SystemExit("ENDE GELÄNDE!")
         rmk.run(speed)
     rmk.brake()
 
-    
-
-
-
-def drb_m(distance,speed,acceleration=900,second_function = None ,dist2=0,speed2=0):
+def drb_m(distance, speed, acceleration=900, second_function=None, dist2=0, speed2=0):
     print("start function")
-    drb.settings(speed,acceleration,90, 500)
+    drb.settings(speed, acceleration, 90, 500)
     print("finish settings")
-    drb.straight(distance,Stop.HOLD,False)
+    drb.straight(distance, Stop.HOLD, False)
     print("move done")
     if second_function:
-        second_function(dist2,speed2)
+        second_function(dist2, speed2)
     while not drb.done():
         if Button.BLUETOOTH in hub.buttons.pressed():
             raise SystemExit("ENDE")
         if Button.CENTER in hub.buttons.pressed():
-            wait(1)
+            wait(1000)
             raise SystemExit("ENDE GELÄNDE!")
-            
+        wait(10)
 
-
-
-def drb_t(angle,speed,acceleration=500,second_function = None,dist2=0,speed2=0):
+def drb_t(angle, speed, acceleration=500, second_function=None, dist2=0, speed2=0):
     print(hub.imu.heading())
-    drb.settings(400,400,speed,acceleration)
-    drb.turn(angle,Stop.HOLD,False)
+    drb.settings(400, 400, speed, acceleration)
+    drb.turn(angle, Stop.HOLD, False)
     if second_function:
-        second_function(dist2,speed2)
+        second_function(dist2, speed2)
     while not drb.done():
         if Button.BLUETOOTH in hub.buttons.pressed():
             raise SystemExit("ENDE")
         if Button.CENTER in hub.buttons.pressed():
-            wait(1)
+            wait(1000)
             raise SystemExit("ENDE GELÄNDE!")
+        wait(10)
 
+def drb_k(radius, angle, speed, acceleration=500, second_function=None, dist2=0, speed2=0):
+    print(f"Starting heading: {hub.imu.heading()}")
+    
+    drb.settings(straight_speed=speed, straight_acceleration=acceleration, 
+                turn_rate=100, turn_acceleration=acceleration)
+    
+    drb.curve(radius, angle, wait=False)
+    
+    if second_function:
+        second_function(dist2, speed2)
+    
+    while not drb.done():
+        if Button.BLUETOOTH in hub.buttons.pressed():
+            raise SystemExit("ENDE")
+        if Button.CENTER in hub.buttons.pressed():
+            wait(1000)
+            raise SystemExit("ENDE GELÄNDE!")
+        wait(10)
+    
+    print(f"Final heading: {hub.imu.heading()}")
 
 def run1(): 
+    watch = StopWatch()
     try:
-        drb_m(800, 900)
+        drb_m(710, 350)
         drb_t(-42, 400)
-        drb_m(150, 400)
+        drb_m(120, 150)
         lmkmove(100, 400)
         wait(100)
-        drb_m(-110, 400)
-        drb_t(42, 500)
-        drb_m(-175, 200)
-        wait(425)
-        lmkmove(390, 650)
-        lmkmove(390,-650)
-        drb_m(-530, 1000)
+        drb_m(-130, 200)
+        drb_t(-110, 200)
+        drb_m(-120, 200)
+        drb_t(30, 200)
+        drb_k(-90, -15, 150,rmkmove(1600, 1200))
+        drb_m(-50, 200)
+        drb_k(-90, -12, 150)
+        drb_m(50,200)
+        lmkmove(300,500)
+        drb_m(-150, 200)
+        lmkmove(550,-500)
+        rmkmove(130,-700)
+        lmkmove(400,500)
+        wait(500)
+        drb_m(140,200)
+        lmkmove(200,-500)
+        rmkmove(300,-700)
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
 
     except SystemExit:
         print("This was a stop!")
@@ -130,57 +131,9 @@ def run1():
     rmk.brake()
     global var
     var = int(hub_menu("2","3","4","5","6","7","8","9","1"))
-    print("var")
 
 def run2():
-    try:
-        drb_m(600,500)
-        rmkmove(500,-600)
-        drb_m(-590,500)
-
-    except SystemExit:
-        print("This was a stop!")
-    drb.stop()
-    lmk.brake()
-    rmk.brake()
-    global var
-    var = int(hub_menu("3","4","5","6","7","8","9","1","2",))
-
-
-def run3():
-    try:
-        drb_m(250, 400)
-        drb_t(89, 400)
-        drb_m(600, 400)
-        drb_t(-30,400)
-        drb_m(180,400)
-        drb_t(-115,400)
-        drb_m(-30,400)
-        lmkmove(220,-400)
-        drb_m(80,400)
-        lmkmove(200,200)
-        wait(100)
-        #lmkmove(50,700)
-        drb_m(-100,400)
-        drb_t(30,400)
-        drb_m(-180,400)
-        drb_t(32,400)
-        drb_m(280,200)
-        wait(400)
-        drb_t(-2.5,400)
-        drb_m(-370,400)
-        rmkmove(6000,-1200)
-
-    except SystemExit:
-        print("This was a stop!")
-    drb.stop()
-    lmk.brake()
-    rmk.brake()
-    global var
-    var = int(hub_menu("4","5","6","7","8","9","1","2","3"))
-
-
-def run4():
+    watch = StopWatch()
     try:
         drb_m(620, 400)
         drb_t(30, 400)
@@ -194,6 +147,70 @@ def run4():
         drb_m(-144, 400)
         drb_t(70, 400)
         drb_m(613, 400)
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
+    except SystemExit:
+        print("This was a stop!")
+    drb.stop()
+    lmk.brake()
+    rmk.brake()
+    global var
+    var = int(hub_menu("3","4","5","6","7","8","9","1","2"))
+
+def run3():
+
+    watch = StopWatch()
+    try:
+        lmkmove(220,1000)
+        lmkmove(-200,1200)
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
+    except SystemExit:
+        print("This was a stop!")
+    drb.stop()
+    lmk.brake()
+    rmk.brake()
+    global var
+    var = int(hub_menu("4","5","6","7","8","9","1","2","3"))
+
+def run4():
+
+    watch = StopWatch()
+    try:
+        
+        drb_m(180, 300)
+        drb_t(-90, 300)
+        drb_m(800, 500)
+        drb_t(-90, 300)
+        drb_m(160, 500)
+        drb_t(-20, 300)
+        lmkmove(850, 1000)#kran
+        
+        drb_m(-250, 400)
+        drb_k(-15, 200, 200)
+        rmkmove(350, 500)
+        drb_m(206, 400)
+        rmkmove(300, -500) #Statur
+        drb_m(-200, 400) 
+        drb_t(130, 400)
+        drb_m(150, 300)
+        drb_m(-
+        
+        
+        300, 300)
+        drb_k(60, -80, 800)
+        drb_m(550, 800)
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
     except SystemExit:
         print("This was a stop!")
     drb.stop()
@@ -202,27 +219,60 @@ def run4():
     global var
     var = int(hub_menu("5","6","7","8","9","1","2","3","4"))
 
-
 def run5():
+    
+    watch = StopWatch()
+    
     try:
-        drb_m(570,400,500)
+        drb_m(200, 300)
         drb.stop()
+        lmk.brake()
+        rmk.brake()
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
     except SystemExit:
         print("This was a stop!")
-    drb.stop()
-    lmk.brake()
-    rmk.brake()
     global var
     var = int(hub_menu("6","7","8","9","1","2","3","4","5"))
-    
-
 
 def run6():
-    try:
 
-        lmkmove(100,100)
-        drb_m(50,500)
+    watch = StopWatch()
+
+    try:
+        drb_m(710, 350)
+        drb_t(-42, 400)
+        drb_m(120, 150)
+        lmkmove(100, 400)
+        wait(100)
+        drb_m(-130, 200)
+        drb_t(-110, 200)
+        drb_m(-120, 200)
+        drb_t(30, 200)
+        drb_k(-90, -15, 150,rmkmove(1600, 1200))
+        #drb_m(50,200)
+        #lmkmove(300,500)
+        drb_m(-50, 200)
+        drb_k(-90, -12, 150)
+        drb_m(50,200)
+        lmkmove(300,500)
+        drb_m(-150, 200)
+        lmkmove(550,-500)
+        rmkmove(130,-700)
+        lmkmove(400,500)
+        wait(500)
+        drb_m(140,200)
+        lmkmove(200,-500)
+        rmkmove(300,-700)
         drb.stop()
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
     except SystemExit:
         print("This was a stop!")
     drb.stop()
@@ -231,85 +281,95 @@ def run6():
     global var
     var = int(hub_menu("7","8","9","1","2","3","4","5","6"))
 
-
 def run7():
+
+    watch = StopWatch()
+
     try:
-        drb_m(-500,900)
+        drb_m(500, 600)
+        rmkmove(110, 400)
+        drb_m(-500, 580)
         drb.stop()
-        lmk.brake()
-        rmk.brake()
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
     except SystemExit:
         print("This was a stop!")
     drb.stop()
     global var
     var = int(hub_menu("8","9","1","2","3","4","5","6","7"))
 
-
 def run8():
+
+    watch = StopWatch()
+
     try:
-        drb_m(200,300)
+        drb_m(200, 300)
         drb.stop()
         lmk.brake()
         rmk.brake()
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
     except SystemExit:
         print("This was a stop!")
     drb.stop()
     global var
     var = int(hub_menu("9","1","2","3","4","5","6","7","8"))
 
-
 def run9():
+
+    watch = StopWatch()
+    
     try:
-        drb_m(50000,900,1500)
+        drb_m(200, 300)
         drb.stop()
         lmk.brake()
         rmk.brake()
+
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
     except SystemExit:
         print("This was a stop!")
     drb.stop()
     global var
     var = int(hub_menu("1","2","3","4","5","6","7","8","9"))
 
-
-var = int(hub_menu("1","2","3","4","5","6","7","8","9",))
+var = int(hub_menu("1","2","3","4","5","6","7","8","9"))
 print(var)
 
 while True:
-    if var ==1:
+    if var == 1:
         print("run1")
         run1() 
-    if var ==2:
+    elif var == 2:
         print("run2")
         run2()
-    if var==3:
+    elif var == 3:
         print("run3")
         run3()
-    if var==4:
+    elif var == 4:
         print("run4")
         run4()
-    if var==5:
+    elif var == 5:
         print("run5")
         run5()
-    if var==6:
+    elif var == 6:
         print("run6")
         run6()
-    if var==7:
+    elif var == 7:
         print("run7")
         run7()
-    if var==8:
+    elif var == 8:
         print("run8")
         run8()
-    if var==9:
+    elif var == 9:
         print("run9")
         run9()
-
-print(x,y)
-
-async def counter():
-    a=1   
-    while True:
-        print(a)
-        a=1
-
-
-print("neu") 
+    wait(100)
