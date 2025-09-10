c4e5e8d980e1958d7748d7d43c9775ba2cb43421
FFLish <fflishrobotics@gmail.com>
2025-09-11 01:28:42 +0200

    Update run2, run4, and run6 functions with revised movement sequences and speed adjustments for enhanced navigation and performance.

--- main.py
+++ main.py
@@ -94,6 +94,10 @@ def drb_k(radius, angle, speed, acceleration=500, second_function=None, dist2=0,
     
     print(f"Final heading: {hub.imu.heading()}")
 
+# -----------------------
+# RUN FUNCTIONS
+# -----------------------
+
 def run1(): 
     watch = StopWatch()
     try:
@@ -106,7 +110,7 @@ def run1():
         drb_t(-110, 200)
         drb_m(-120, 200)
         drb_t(30, 200)
-        drb_k(-90, -15, 150,rmkmove(1600, 1200))
+        drb_k(-90, -15, 150,rmkmove, 1600, 1200)
         drb_m(-50, 200)
         drb_k(-90, -12, 150)
         drb_m(50,200)
@@ -135,18 +139,27 @@ def run1():
 def run2():
     watch = StopWatch()
     try:
-        drb_m(620, 400)
-        drb_t(30, 400)
-        drb_m(170, 400)
-        lmkmove(100, 500)
-        drb_m(40, 500)
-        rmkmove(180, 500)
-        drb_m(40, 500)
-        rmkmove(180, -500)
-        drb_t(46, 400)
-        drb_m(-144, 400)
-        drb_t(70, 400)
-        drb_m(613, 400)
+        drb_m(320,300)
+        drb_t(90,400)
+        drb_m(530,300)
+        drb_t(45,300)
+        drb_m(210,150)
+        drb_t(-5,200)
+        wait(500)
+        lmkmove(150,700)
+        rmkmove(250,300)
+        wait(500)
+        rmkmove(270,-300)
+        drb_m(-30,200)
+        drb_t(47,100)
+        drb_m(-150,200)
+        drb_m(50,200)
+        drb_t(90,100)
+        drb_m(270,200)
+        drb_t(45,100)
+        drb_m(200,200)
+        drb_k(8,-110,200)
+        drb_m(400,300)
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -181,34 +194,35 @@ def run3():
     var = int(hub_menu("4","5","6","7","8","9","1","2","3"))
 
 def run4():
-
     watch = StopWatch()
     try:
-        
         drb_m(185, 300)
         drb_t(-90, 300)
         drb_m(720, 500)
         drb_t(-90, 300)
         drb_m(170, 500)
         drb_t(-20, 300)
-        lmkmove(850, 1000)#kran    
+        lmkmove(850, 1000)    
 
         drb_m(-260, 400)
-        drb_t(-205,300)
+        drb_t(-210,300)
         rmkmove(650, -1000)
         drb_m(120, 400)
-        rmkmove(700, 400) #Statur
+        rmkmove(260, 400) 
+        drb_t(20, 400)
 
-        drb_m(-210, 400) 
-        drb_t(140, 400)
-        drb_m(75, 300)
+        drb_m(-150, 400) 
+        rmkmove(450, 400)
+        drb_t(130, 400)
+        drb_m(150, 300)
         drb_t(85, 300)
-        drb_m(-180, 100) #Wage
-        drb_m(130,270)   #One way door
+        drb_m(-190, 100)
+        drb_m(140,270)
+        
         drb_t(90,300)
-        drb_m(600,400)
-        drb_t(-45, 400)
-        drb_m(500, 1000)
+        drb_m(300,400)
+        drb_t(-15, 400)
+        drb_m(700, 1000)
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -223,9 +237,7 @@ def run4():
     var = int(hub_menu("5","6","7","8","9","1","2","3","4"))
 
 def run5():
-    
     watch = StopWatch()
-    
     try:
         drb_m(800, 600)
         drb_t(-42, 300)
@@ -252,34 +264,19 @@ def run5():
     var = int(hub_menu("6","7","8","9","1","2","3","4","5"))
 
 def run6():
-
     watch = StopWatch()
-
     try:
-        drb_m(710, 350)
-        drb_t(-42, 400)
-        drb_m(120, 150)
-        lmkmove(100, 400)
-        wait(100)
-        drb_m(-130, 200)
-        drb_t(-110, 200)
-        drb_m(-120, 200)
-        drb_t(30, 200)
-        drb_k(-90, -15, 150,rmkmove(1600, 1200))
-        #drb_m(50,200)
-        #lmkmove(300,500)
-        drb_m(-50, 200)
-        drb_k(-90, -12, 150)
-        drb_m(50,200)
-        lmkmove(300,500)
-        drb_m(-150, 200)
-        lmkmove(550,-500)
-        rmkmove(130,-700)
-        lmkmove(400,500)
-        wait(500)
-        drb_m(140,200)
-        lmkmove(200,-500)
-        rmkmove(300,-700)
+        drb_m(770, 400)
+        drb_k(60, 30, 300)
+        drb_m(140, 400)
+        drb_k(70, 60, 300)
+        drb_m(130, 400)
+        rmkmove(280, 400)
+        lmkmove(35, -500)
+        rmkmove(250, -1000)
+        drb_m(-130, 400)
+        drb_t(100, 300)
+        drb_m(800, 400)
         drb.stop()
 
         elapsed_time = watch.time()
@@ -295,9 +292,7 @@ def run6():
     var = int(hub_menu("7","8","9","1","2","3","4","5","6"))
 
 def run7():
-
     watch = StopWatch()
-
     try:
         drb_m(500, 600)
         rmkmove(110, 400)
@@ -315,9 +310,7 @@ def run7():
     var = int(hub_menu("8","9","1","2","3","4","5","6","7"))
 
 def run8():
-
     watch = StopWatch()
-
     try:
         drb_m(200, 300)
         drb.stop()
@@ -335,24 +328,61 @@ def run8():
     var = int(hub_menu("9","1","2","3","4","5","6","7","8"))
 
 def run9():
-
     watch = StopWatch()
-    
     try:
-        drb_m(200, 300)
+        #batterie= hub.battery.voltage
+        #print(batterie)
+        warn_threshold = 20  # Warnung ab 20%
+
+# Werte für Batterie-Prozent-Berechnung
+        max_voltage = 7.2
+        min_voltage = 6.0
+
+        while True:
+    # Spannung abfragen
+            voltage = hub.battery.voltage()
+    
+    # Batterieschätzung in Prozent
+            battery_percent = max(0, min(100, (voltage - min_voltage) / (max_voltage - min_voltage) * 100))
+            print(battery_percent)
+            wait(1000)
+        '''voltage = hub.battery.voltage()
+        percent = int((voltage - 6000) / (8400 - 6000) * 100)
+        percent = max(0, min(100, percent))
+        hub.display.number(percent)
+        print("Batterie :", percent, "%")
+
+      
+        #drb_m(100 0, 1000)  
+        
+        while True:
+
+            voltage = hub.battery.voltage()
+            percent = int((voltage - 6000) / (8400 - 6000) * 100)
+            percent = max(0, min(100, percent))
+            hub.display.number(percent)
+            
+        '''
+        if Button.BLUETOOTH in hub.buttons.pressed():
+            raise SystemExit("ENDE")
+        if Button.CENTER in hub.buttons.pressed():
+            wait(1000)
+            raise SystemExit("ENDE GELÄNDE!")
+                
+            wait(100)
+
+    except SystemExit:
+        print("This was a stop!")
+    finally:
         drb.stop()
         lmk.brake()
         rmk.brake()
 
-        elapsed_time = watch.time()
-        elapsed_seconds = elapsed_time / 1000
-        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
-
-    except SystemExit:
-        print("This was a stop!")
-    drb.stop()
+    
     global var
     var = int(hub_menu("1","2","3","4","5","6","7","8","9"))
+
+
 
 var = int(hub_menu("1","2","3","4","5","6","7","8","9"))
 print(var)
