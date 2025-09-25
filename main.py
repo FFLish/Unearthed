cc2cdfe35a783b06fe89629de9b7e1da3ef071c4
FFLish <fflishrobotics@gmail.com>
2025-09-25 14:37:03 +0200

    Add test.py with movement functions and run sequences for enhanced robot control

--- main.py
+++ main.py
@@ -101,28 +101,30 @@ def drb_k(radius, angle, speed, acceleration=500, second_function=None, dist2=0,
 def run1(): 
     watch = StopWatch()
     try:
-        drb_m(710, 350)
-        drb_t(-42, 400)
-        drb_m(120, 150)
-        lmkmove(100, 400)
-        wait(100)
-        drb_m(-130, 200)
-        drb_t(-110, 200)
-        drb_m(-120, 200)
-        drb_t(30, 200)
-        drb_k(-90, -15, 150,rmkmove, 1600, 1200)
-        drb_m(-50, 200)
-        drb_k(-90, -12, 150)
-        drb_m(50,200)
-        lmkmove(300,500)
-        drb_m(-150, 200)
-        lmkmove(550,-500)
-        rmkmove(130,-700)
-        lmkmove(400,500)
-        wait(500)
-        drb_m(140,200)
-        lmkmove(200,-500)
-        rmkmove(300,-700)
+        drb_m(705, 350)
+        drb_t(-45, 300)
+        drb_m(100, 300)
+        drb_t(-45, 300)
+        drb_m(450, 300)
+        drb_t(90, 300)
+        drb_m(190, 150)
+        rmkmove(375, -300)
+        drb_m(50, 150)
+        rmkmove(450, 300)
+        drb_m(-130, 300)
+        lmkmove(260, 200)
+        drb_m(-100, 300)
+        drb_t(-90, 300)
+        drb_m(-200, 300)
+        drb_t(45, 300)
+        drb_m(-200, 200) #Aktuelle Angebote hochheben
+        drb_m(100,300)
+        drb_t(130,300)
+        drb_m(100,300)
+        drb_t(70,300)
+        drb_m(200,300)
+        drb_t(30,300)
+        drb_m(500,400)
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -139,27 +141,27 @@ def run1():
 def run2():
     watch = StopWatch()
     try:
-        drb_m(320,300)
-        drb_t(90,400)
-        drb_m(530,300)
+        print("hallo")
+        drb_m(320,900)
+        drb_t(90,700)
+        drb_m(530,500)
         drb_t(45,300)
-        drb_m(210,150)
-        drb_t(-5,200)
+        drb_m(210,500)
+        drb_t(-5,500)
         wait(500)
         lmkmove(150,700)
-        rmkmove(250,300)
+        rmkmove(250,600)
         wait(500)
-        rmkmove(270,-300)
-        drb_m(-30,200)
-        drb_t(47,100)
-        drb_m(-150,200)
-        drb_m(50,200)
-        drb_t(90,100)
-        drb_m(270,200)
-        drb_t(45,100)
-        drb_m(200,200)
-        drb_k(8,-110,200)
-        drb_m(400,300)
+        rmkmove(370,-300)
+        drb_m(-30,700)
+        drb_t(30,500)
+        drb_m(-140,700)
+        drb_t(90,500)
+        drb_m(210,800)
+        drb_t(65,500)
+        drb_m(270,500)
+        drb_k(110,-120,500)
+        drb_m(400,600)
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -176,13 +178,14 @@ def run2():
 def run3():
     try:
         drb_m(430,300)
-        lmkmove(220,-400)
+        lmkmove(220,-800)
         wait(200)
         lmkmove(220,200)
-        lmkmove(220,-400)
+        lmkmove(220,-800)
         wait(200)
         lmkmove(220,200)
-        lmkmove(220,-400)
+        lmkmove(220,-800)
+        wait(200)
         lmkmove(220,200)
         drb_m(-430,300)
     except SystemExit:
@@ -196,9 +199,7 @@ def run3():
 def run4():
     watch = StopWatch()
     try:
-        drb_m(185, 300)
-        drb_t(-90, 300)
-        drb_m(720, 500)
+        drb_m(870, 500)
         drb_t(-90, 300)
         drb_m(170, 500)
         drb_t(-20, 300)
@@ -213,9 +214,9 @@ def run4():
 
         drb_m(-150, 400) 
         rmkmove(450, 400)
-        drb_t(130, 400)
+        drb_t(140, 400)
         drb_m(150, 300)
-        drb_t(85, 300)
+        drb_t(90, 300)
         drb_m(-190, 100)
         drb_m(140,270)
         
@@ -239,20 +240,19 @@ def run4():
 def run5():
     watch = StopWatch()
     try:
-        drb_m(800, 600)
-        drb_t(-42, 300)
-        drb_m(180, 200)
-        lmkmove(100, 600)
-        drb_m(-110, 500)
-        drb_t(42, 300)
-        drb_m(-160, 150)
-        lmkmove(280, 700)
-        wait(1200)
-        lmkmove(180, -600)
-        drb_m(-540, 600)
+        drb_m(770, 400)
+        drb_k(60, 30, 300)
+        drb_m(140, 400)
+        drb_k(70, 60, 300)
+        drb_m(130, 400)
+        rmkmove(280, 400)
+        lmkmove(35, -500)
+        rmkmove(250, -1000)
+        drb_m(-130, 400)
+        drb_t(100, 300)
+        drb_m(800, 400)
         drb.stop()
-        lmk.brake()
-        rmk.brake()
+        
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -266,18 +266,24 @@ def run5():
 def run6():
     watch = StopWatch()
     try:
-        drb_m(770, 400)
-        drb_k(60, 30, 300)
-        drb_m(140, 400)
-        drb_k(70, 60, 300)
-        drb_m(130, 400)
-        rmkmove(280, 400)
-        lmkmove(35, -500)
-        rmkmove(250, -1000)
-        drb_m(-130, 400)
-        drb_t(100, 300)
-        drb_m(800, 400)
+        
+
+        drb_m(800, 800)
+        drb_t(-42, 500)
+        drb_m(180, 300)
+        lmkmove(100, 600)
+        drb_m(-110, 500)
+        drb_t(42, 300)
+        drb_m(-130, 150)
+        rmkmove(100, 700)#Landkarte hoch
+        drb_m(-35, 150)
+        wait(500)
+        lmkmove(290, 800)#dreizack runter
+        lmkmove(190, -900)
+        drb_m(-600, 1000)
         drb.stop()
+        lmk.brake()
+        rmk.brake()
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -294,9 +300,9 @@ def run6():
 def run7():
     watch = StopWatch()
     try:
-        drb_m(500, 600)
-        rmkmove(110, 400)
-        drb_m(-500, 580)
+        drb_m(500, 900)
+        rmkmove(120, 800)
+        drb_m(-500, 900)
         drb.stop()
 
         elapsed_time = watch.time()
@@ -312,7 +318,11 @@ def run7():
 def run8():
     watch = StopWatch()
     try:
-        drb_m(200, 300)
+        drb_m(540, 1000)
+        drb_m(-270, 1000)
+
+
+       
         drb.stop()
         lmk.brake()
         rmk.brake()
