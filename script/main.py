d727e38448cebf7932a143865aceddcc9b8c2933
FFLish <fflishrobotics@gmail.com>
2025-11-20 15:39:45 +0100

    funktioniert

--- script/main.py
+++ script/main.py
@@ -94,30 +94,22 @@ def drb_k(radius, angle, speed, acceleration=500, second_function=None, dist2=0,
         wait(10)
 def run1(): 
     try:
-        drb_m(705, 350)
-        drb_t(-45, 300)
-        drb_m(100, 300)
-        drb_t(-45, 300)
-        drb_m(450, 300)
-        drb_t(90, 300)
-        drb_m(190, 150)
-        rmkmove(375, -300)
-        drb_m(50, 150)
-        rmkmove(450, 300)
-        drb_m(-130, 300)
-        lmkmove(260, 200)
-        drb_m(-100, 300)
-        drb_t(-90, 300)
-        drb_m(-200, 300)
-        drb_t(45, 300)
-        drb_m(-200, 200) #Aktuelle Angebote hochheben
-        drb_m(100,300)
-        drb_t(130,300)
-        drb_m(100,300)
-        drb_t(70,300)
-        drb_m(200,300)
-        drb_t(30,300)
-        drb_m(500,400)
+        drb_m(100,600)
+        drb_k(100,-60,200)
+        drb_m(400,500)
+        drb_k(300,-62,900)
+        drb_m(20,1000)
+        drb_k(87,85,500)
+        rmkmove(800,900)
+        drb_m(-65,500)
+        rmkmove(800,-900);  lmkmove(200, 800)
+        drb_m(-25, 1000)
+        drb_k(-40,-62,900)
+        drb_k(270,110,500)
+        
+        drb_m(480,900)
+        drb_k(90,-85,500)
+
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -129,32 +121,24 @@ def run1():
     lmk.brake()
     rmk.brake()
     global var
-    var = int(hub_menu("2","3","4","5","6","7","8","9","1"))
+    var = int(hub_menu("2","3","4","5","6","7","8","1"))
 
 def run2():
     try:
         print("hallo")
-        drb_m(320,900)
-        drb_t(90,700)
-        drb_m(530,500)
-        drb_t(45,300)
-        drb_m(210,500)
-        drb_t(-5,500)
-        wait(500)
-        lmkmove(150,700)
-        rmkmove(250,600)
-        wait(500)
-        rmkmove(370,-300)
-        drb_m(-30,700)
-        drb_t(30,500)
-        drb_m(-140,700)
-        drb_t(90,500)
-        drb_m(210,800)
-        drb_t(65,500)
-        drb_m(270,500)
-        drb_k(110,-120,500)
-        drb_m(400,600)
-
+        drb_m(770, 400)
+        drb_k(60, 30, 300)
+        drb_m(140, 400)
+        drb_k(70, 60, 300)
+        drb_m(130, 400)
+        rmkmove(280, 400)
+        lmkmove(35, -500)
+        rmkmove(250, -1000)
+        drb_m(-130, 400)
+        drb_t(100, 300)
+        drb_m(800, 400)
+        drb.stop()
+        
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
         print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
@@ -165,7 +149,7 @@ def run2():
     lmk.brake()
     rmk.brake()
     global var
-    var = int(hub_menu("3","4","5","6","7","8","9","1","2"))
+    var = int(hub_menu("3","4","5","6","7","8","1","2"))
 
 def run3():
     try:
@@ -182,6 +166,32 @@ def run3():
 
 
 def run4():
+    try:
+        drb_m(50,500)
+        drb_t(-65,500)
+        drb_k(550,20,500)
+        drb_m(330,500)
+        rmkmove(330,500)
+        drb_m(-150, 500)
+        drb_k(700,-5,500)
+        rmkmove(330,-500)
+        drb_k(400,5,500)
+        lmkmove(350, -500)
+        drb_k(-400,20,500)
+        drb_m(-450,500)
+        elapsed_time = watch.time()
+        elapsed_seconds = elapsed_time / 1000
+        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
+
+    except SystemExit:
+        print("This was a stop!")
+    drb.stop()
+    lmk.brake()
+    rmk.brake()
+    global var
+    var = int(hub_menu("5","6","7","8","1","2","3","4"))
+
+def run5():
     watch = StopWatch()
     try:
         drb_k(230, -90, 400)
@@ -196,12 +206,14 @@ def run4():
         drb_t(133, 300)
         rmkmove(560, 900)
         drb_m(320, 400)
-        rmkmove(480, -500)
-        drb_k(-100, 60, -300)
+        rmkmove(210, -600)
+        drb_t(22, 300)
+        drb_k(-100, 82, -300)
         drb_m(780, 600)
 
         drb_m(5, 500)
-        drb_t(-45, 400)
+        
+
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
         print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
@@ -214,22 +226,21 @@ def run4():
     global var
     var = int(hub_menu("6","7","8","1","2","3","4","5"))
 
-def run5():
+def run6():
     watch = StopWatch()
     try:
-        drb_m(770, 400)
-        drb_k(60, 30, 300)
-        drb_m(140, 400)
-        drb_k(70, 60, 300)
-        drb_m(130, 400)
-        rmkmove(280, 400)
-        lmkmove(35, -500)
-        rmkmove(250, -1000)
-        drb_m(-130, 400)
-        drb_t(100, 300)
-        drb_m(800, 400)
-        drb.stop()
-        
+        drb_m(750, 500)
+        drb_t(30, 500)
+        drb_m(160, 400)
+        drb_k(70, 60, 500)
+        drb_m(150, 500)
+        lmkmove(40,-400)
+        rmkmove(200,500)
+        rmkmove(190,-500)
+        drb_m(-170,500)
+        drb_t(100,500)
+        drb_m(800,1000)  
+
 
         elapsed_time = watch.time()
         elapsed_seconds = elapsed_time / 1000
@@ -241,14 +252,13 @@ def run5():
     lmk.brake()
     rmk.brake()
     global var
-    var = int(hub_menu("6","7","8","9","1","2","3","4","5"))
+    var = int(hub_menu("7","8","1","2","3","4","5","6"))
 
-def run6():
+def run7():
     watch = StopWatch()
     try:
-        
 
-        drb_m(800, 800)
+        drb_m(780, 800)
         drb_t(-42, 500)
         drb_m(180, 300)
         lmkmove(100, 600)
@@ -265,21 +275,8 @@ def run6():
         lmk.brake()
         rmk.brake()
 
-        elapsed_time = watch.time()
-        elapsed_seconds = elapsed_time / 1000
-        print("Verbrauchte Zeit:", elapsed_seconds, "Sekunden")
 
-    except SystemExit:
-        print("This was a stop!")
-    drb.stop()
-    lmk.brake()
-    rmk.brake()
-    global var
-    var = int(hub_menu("7","8","9","1","2","3","4","5","6"))
 
-def run7():
-    watch = StopWatch()
-    try:
         drb_m(500, 900)
         rmkmove(120, 800)
         drb_m(-500, 900)
@@ -295,11 +292,11 @@ def run7():
 
 def run8():
     try:
-        drb_m(540, 1000)
-        wait(500)
-        drb_m(-270, 1000)
 
-
+        drb_m(500, 900)
+        rmkmove(120, 800)
+        drb_m(-500, 900)
+        drb.stop()
        
         drb.stop()
         lmk.brake()
@@ -308,67 +305,12 @@ def run8():
         print("This was a stop!")
     drb.stop()
     global var
-    var = int(hub_menu("9","1","2","3","4","5","6","7","8"))
-
-def run9():
-    watch = StopWatch()
-    try:
-        #batterie= hub.battery.voltage
-        #print(batterie)
-        warn_threshold = 20  # Warnung ab 20%
-
-# Werte für Batterie-Prozent-Berechnung
-        max_voltage = 7.2
-        min_voltage = 6.0
-
-        while True:
-    # Spannung abfragen
-            voltage = hub.battery.voltage()
+    var = int(hub_menu("1","2","3","4","5","6","7","8"))
     
-    # Batterieschätzung in Prozent
-            battery_percent = max(0, min(100, (voltage - min_voltage) / (max_voltage - min_voltage) * 100))
-            print(battery_percent)
-            wait(1000)
-        '''voltage = hub.battery.voltage()
-        percent = int((voltage - 6000) / (8400 - 6000) * 100)
-        percent = max(0, min(100, percent))
-        hub.display.number(percent)
-        print("Batterie :", percent, "%")
-
-      
-        #drb_m(100 0, 1000)  
-        
-        while True:
-
-            voltage = hub.battery.voltage()
-            percent = int((voltage - 6000) / (8400 - 6000) * 100)
-            percent = max(0, min(100, percent))
-            hub.display.number(percent)
-            
-        '''
-        if Button.BLUETOOTH in hub.buttons.pressed():
-            raise SystemExit("ENDE")
-        if Button.CENTER in hub.buttons.pressed():
-            wait(1000)
-            raise SystemExit("ENDE GELÄNDE!")
-                
-            wait(100)
-
-    except SystemExit:
-        print("This was a stop!")
-    finally:
-        drb.stop()
-        lmk.brake()
-        rmk.brake()
-
-    
-    global var
-    var = int(hub_menu("1","2","3","4","5","6","7","8","9"))
-
-
-
-var = int(hub_menu("1","2","3","4","5","6","7","8","9"))
-print(var)
+try:
+    var = int(hub_menu("1","2","3","4","5","6","7","8"))
+except Exception:
+    var = 1
 
 while True:
     if var == 1:
